c7e7de68172d6ff2ad59ee995142f9e8e4559b21
Polma Tambunan <plmtmbnn@gmail.com>
2018-07-06 20:55:52 +0700

    fixed minor bugs

--- src/org/sia/driver/Sia.java
+++ src/org/sia/driver/Sia.java
@@ -1,10 +1,13 @@
 package org.sia.driver;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  *
@@ -21,6 +24,13 @@ public class Sia extends Application
         Scene scene = new Scene(parent, 1000, 700);
         primaryStage.setScene(scene);
         primaryStage.show();
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+        @Override
+        public void handle(WindowEvent event) {
+            Platform.exit();
+            System.exit(0);
+        }
+        });
     }
     
 }
